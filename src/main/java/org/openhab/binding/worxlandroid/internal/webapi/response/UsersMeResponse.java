6bfac238ebe5085c7c550090d2f98660b3d1da2e
nibi79 <mail@nilsbilling.de>
2020-03-13 23:19:09 +0100

    Add properties to bridge, cleanup

--- src/main/java/org/openhab/binding/worxlandroid/internal/webapi/response/UsersMeResponse.java
+++ src/main/java/org/openhab/binding/worxlandroid/internal/webapi/response/UsersMeResponse.java
@@ -12,6 +12,9 @@
  */
 package org.openhab.binding.worxlandroid.internal.webapi.response;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 import org.eclipse.jdt.annotation.NonNullByDefault;
 
 /**
@@ -28,6 +31,29 @@ public class UsersMeResponse extends WebApiResponse {
      */
     public UsersMeResponse(String jsonResponse) {
         super(jsonResponse);
+    }
+
+    /**
+     * @return response data as property list
+     */
+    public Map<String, String> getDataAsPropertyList() {
+        Map<String, String> props = new LinkedHashMap<>();
+        props.put(MN_ID, getMemberDataAsString(MN_ID));
+        props.put(MN_EMAIL, getMemberDataAsString(MN_EMAIL));
+        props.put(MN_NAME, getMemberDataAsString(MN_NAME));
+        props.put(MN_SURNAME, getMemberDataAsString(MN_SURNAME));
+        props.put(MN_USER_TYPE, getMemberDataAsString(MN_USER_TYPE));
+        props.put(MN_LOCALE, getMemberDataAsString(MN_LOCALE));
+        props.put(MN_PUSHNOTIFICATIONS, getMemberDataAsString(MN_PUSHNOTIFICATIONS));
+        props.put(MN_LOCATION_LATITUDE, getMemberDataAsString(MN_LOCATION_LATITUDE));
+        props.put(MN_LOCATION_LONGITUDE, getMemberDataAsString(MN_LOCATION_LONGITUDE));
+        props.put(MN_TERMSOFUSEAGREED, getMemberDataAsString(MN_TERMSOFUSEAGREED));
+        props.put(MN_COUNTRYID, getMemberDataAsString(MN_COUNTRYID));
+        props.put(MN_MQTTENDPOINT, getMemberDataAsString(MN_MQTTENDPOINT));
+        props.put(MN_ACTIONSONGOOGLEPINCODE, getMemberDataAsString(MN_ACTIONSONGOOGLEPINCODE));
+        props.put(MN_CREATEDAT, getMemberDataAsString(MN_CREATEDAT));
+        props.put(MN_UPDATEDAT, getMemberDataAsString(MN_UPDATEDAT));
+        return props;
     }
 
 }
